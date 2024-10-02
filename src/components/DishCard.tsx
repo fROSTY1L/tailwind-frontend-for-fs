6fa8c484f431f65c51b6a72c6c99494f0df90b75
fROSTY1L <124553997+fROSTY1L@users.noreply.github.com>
2024-10-03 01:45:07 +0300

    without comms

--- src/components/DishCard.tsx
+++ src/components/DishCard.tsx
@@ -16,7 +16,7 @@ const DishCard: React.FC<Dish> = ({ name, price, img, onAddToCart }) => {
           onClick={onAddToCart}
           className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full transition duration-300 ease-in-out transform hover:scale-105"
         >
-          Добавить
+          Добавить 123
         </button>
       </div>
     </div>
